b167e43c67d73c31d7edd5c47d3e19fa10f3826c
Hunter Zhao <cxzhao7@gmail.com>
2022-05-03 20:53:40 -0300

    Use TrackBar

--- Week2/Assignment/submission.py
+++ Week2/Assignment/submission.py
@@ -2,28 +2,40 @@ import cv2
 import math
 
 
-point1 = []
-point2 = []
-def drawRectangle (action, x, y, flags, param):
-    global point1, point2
+topleft = []
+bottomright = []
+def drawRectangle (action, x, y, flags, userdata):
+    # Referencing global variables
+    global topleft, bottomright
+    # Action to be taken when left mouse button is pressed
     if action == cv2.EVENT_LBUTTONDOWN:
-        point1 = [(x,y)]
+        topleft = [(x,y)]
+        # Mark the vertex
+        cv2.circle(img, topleft[0], 1, (200, 100, 0), 2, cv2.LINE_AA)
+    # Action to be taken when left mouse button is released
     elif action == cv2.EVENT_LBUTTONUP:
-        point2 = [(x,y)]
-        cv2.rectangle(img, point1[0], point2[0], (200,100,0),3, lineType=cv2.LINE_AA)
+        bottomright = [(x,y)]
+        # Mark the vertex
+        cv2.circle(img, bottomright[0], 1, (200, 100, 0), 2, cv2.LINE_AA)
+        # Draw the square
+        cv2.rectangle(img, topleft[0], bottomright[0], (200,100,0),3, lineType=cv2.LINE_AA)
         cv2.imshow('Window', img)
-        crop = img[point1[0][1]:point2[0][1],point1[0][0]:point2[0][0]]
+        crop = img[topleft[0][1]:bottomright[0][1],topleft[0][0]:bottomright[0][0]]
         cv2.imwrite('face.png',crop)
 img = cv2.imread('sample.jpg',1)
+# Make a dummy image, will be useful to clear the drawing
 dummy = img.copy()
 cv2.namedWindow('Window')
+# highgui function called when mouse events occur
 cv2.setMouseCallback('Window',drawRectangle)
 k = 0
+# loop until escape character is pressed
 while k!=27:
     cv2.imshow('Window',img)
     cv2.putText(img,"Choose center, and drag, Press ESC to exit and c to clear",
                 (10,30), cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,255,255),2)
     k = cv2.waitKey(20) & 0xFF
+    # Another way of cloning
     if k==99:
         img = dummy.copy()
 
